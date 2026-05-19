4a5fcada0d79ad2f712dc98552d0e2ecd8ec4990
AtharvSaraf1 <atharvsaraf8@gmail.com>
2026-05-19 15:50:58 +0530

    solved api documentation error and spelling

--- src/utils/swagger.js
+++ src/utils/swagger.js
@@ -10,7 +10,7 @@ const options = {
         },
 
         servers: [{
-            url: "http://localhost:5000",
+            url: "https://backend-bachatgat.onrender.com",
         }, ],
 
         components: {
