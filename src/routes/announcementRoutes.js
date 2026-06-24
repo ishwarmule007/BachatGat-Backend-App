d03adb68405066384a1f820fecc8a9c9395a6c7e
AtharvSaraf1 <atharvsaraf8@gmail.com>
2026-06-24 16:39:30 +0530

    importing the right middleware in the routes

--- src/routes/announcementRoutes.js
+++ src/routes/announcementRoutes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-const authMiddleware = require("../middlewares/authMiddleware");
-
+const presidentMiddleware = require('../middlewares/presidentMiddleware');
+const secretaryMiddleware = require('../middlewares/secretaryMiddleware');
+const treasurerMiddleware = require('../middlewares/treasurerMiddleware');
 const {
     createAnnouncement,
     getGroupAnnouncements,
